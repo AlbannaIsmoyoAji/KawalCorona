c1343442c4b1c8594461360ee4c333d2f4ad703f
AIA240300 <60763704+AlbannaIsmoyoAji@users.noreply.github.com>
2020-04-21 14:52:48 +0700

    Mini Project

--- app/src/androidTest/java/com/raizen/fighter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/raizen/fighter/ExampleInstrumentedTest.java
@@ -3,12 +3,12 @@ package com.raizen.fighter;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.azhar.indonews", appContext.getPackageName());
+        assertEquals("com.raizen.fighter", appContext.getPackageName());
     }
 }
